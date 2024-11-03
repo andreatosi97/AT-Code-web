2ea2ce6eddbe6d9fa390c485333f1d4963919560
Andrea Tosi <andrea.tosi120397@gmail.com>
2024-11-03 06:25:48 +0100

    Angular default structure

--- js/changenb.js
+++
@@ -1,8 +0,0 @@
-function changenb(id) {
-    var el = document.getElementById(id);
-    el.style.backgroundColor = "transparent";
-    el.style.borderBottom = "none";
-    el.style.boxShadow = "none";
-    // var ob = document.getElementsByClassName(cl)[0];
-    // ob.style.display = "none";
-}
